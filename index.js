693ec267f058a4b91dbfe2e753fd0b428f3b202b
hanjo77 <hanjo77@gmail.com>
2015-04-28 01:32:39 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+var http = require('http');
+var mqtt    = require('mqtt');
+var client  = mqtt.connect('mqtt://hanjo.synology.me:1883');
+
+var express = require('express'),                                                                                        
+    http = require('http');                                                                                              
+                                                                                                                         
+var app = express();         
+
+var taxis = [];
+                                                                                                                         
+app.engine('handlebars', require('express3-handlebars')({ defaultLayout: 'main' }));                                     
+app.set('view engine', 'handlebars');                                                                                    
+                                                                                                                         
+app.use(require('morgan')('dev'));                                                                                       
+                                                                                                                         
+app.use(express.static(__dirname + '/public'));                                                                          
+
+client.on('connect', function () {
+  client.subscribe('presence');
+});
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer 
+  message = JSON.parse(message);
+  var taxi = {
+		id: message.driver,
+		name: message.car,
+		amenities: "",
+		loc: { lat: message.gps.latitude, lng: message.gps.longitude },
+  };
+  var added = false;
+  for (var i = 0; i < taxis.length; i++) {
+
+  	if (taxis[i].id == message.driver) {
+
+  		taxis[i] = taxi;
+  		added = true;
+  	}
+  }
+  if (!added) {
+
+	  taxis.push(taxi);
+  }
+  console.log(taxis[0]);                                                                                                                                                                                 
+                                                                                                                         
+});
+
+app.get('/', function(req, res){                                                                                         
+    res.render('home');                                                                                                  
+});                                                                                                                      
+                                                                                                                         
+app.get('/error', function(req, res){                                                                                    
+    throw new Error('Whoops!');                                                                                          
+});                                                                                                                      
+    
+app.get('/api/easycab', function(req, res){                                                                    
+    res.json(taxis);                                                                                                                  
+});                                                                                                                      
+
+/*                                                                                                                         
+app.get('/partials/park-info/:id', function(req, res){                                                                   
+    getParks(function(parks){                                                                                            
+        res.render('partials/park-info', { park: parks.byId[req.params.id], layout: null });                             
+    });                                                                                                                  
+}); */                                                                                                                     
+                                                                                                                         
+app.use(function(req, res){                                                                                              
+    res.render('404');                                                                                                   
+});                                                                                                                      
+                                                                                                                         
+app.use(function(err, req, res, next){                                                                                   
+    console.error('Server error: ' + err.stack);                                                                         
+    res.render('500');                                                                                                   
+});                                                                                                                      
+                                                                                                                         
+app.listen(3000, function(){                                                                                             
+    console.log('listening on port 3000');                                                                               
+});                                                                                                                      
+        
